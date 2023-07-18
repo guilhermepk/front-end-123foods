396fd3d53c4ff81eca3b8d1308cd80e31531980c
Gabriel Taschner Eddine <gabrieltaschnereddine@gmail.com>
2023-07-18 20:34:18 -0300

    fazendo nav

---
+++ teste/src/componentes/Footer/footer.js
@@ -0,0 +1,13 @@
+import './footer.css';
+
+function Footer(){
+    return(
+        <footer>
+            <div className='container'>
+                <p>Este é meu footer</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
