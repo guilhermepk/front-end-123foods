ee9dfafc4a10315373ed992bfc587b5b3a093799
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-07-19 20:42:49 -0300

    Descobre ae oqq eu fiz parcero

--- teste/src/App.js
+++ teste/src/App.js
@@ -7,16 +7,21 @@ import {
 import Home from './views/Home';
 import PaginaExemplo from './views/PaginaExemplo';
 
+import Navbar from './componentes/Navbar/Navbar';
+import Footer from './componentes/Footer/footer';
+
 const App = () => {
   return (
-    <Router>
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path='/pagina-exemplo' element={<PaginaExemplo/>}/>
-      </Routes>
+      <Navbar/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path='/pagina-exemplo' element={<PaginaExemplo/>}/>
+        </Routes>
+      </Router>
+      <Footer/>
     </div>
-    </Router>
   );
 }
 
--- teste/src/views/Home.js
+++ teste/src/views/Home.js
@@ -1,20 +1,16 @@
 import { Link } from "react-router-dom";
 
 import Banner from '../componentes/Banner/Banner';
-import Navbar from '../componentes/Navbar/Navbar';
 import Filtrocat from '../componentes/Filtrocategorias/filtrocategorias';
-import Footer from '../componentes/Footer/footer';
 import FormularioCadastroUser from '../componentes/Formulariocadastro/formulariocadastro';
 
 const Home = () => {
     return (
         <div>
-            <Navbar/>
             <Banner/>
             <Filtrocat/>
-            <p> <Link to='/pagina-exemplo'> Página de Exemplo (Clique aqui) </Link> </p>
+            <h1> <Link to='/pagina-exemplo'> Página de Exemplo (Clique aqui) </Link> </h1>
             <FormularioCadastroUser/>
-            <Footer/>
         </div>
     );
 }
