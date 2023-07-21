9845f49188ae3def98ecc1f3fbd944c6f21b983c
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-07-21 12:20:34 -0300

    formprodu

--- teste/src/views/admin/AdminRouteGuard.js
+++ teste/src/views/admin/AdminRouteGuard.js
@@ -1,6 +1,3 @@
-import React from "react";
-import { Navigate, Route } from "react-router-dom";
-
 const isAdmin = () => {
   const userInfo = localStorage.getItem("userInfo");
   return userInfo ? JSON.parse(userInfo).admin : false;
