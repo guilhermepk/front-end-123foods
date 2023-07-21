6e7d39e74cea62b4d339147f08e3184e4b316af5
viniggj2005 <102488557+viniggj2005@users.noreply.github.com>
2023-07-21 12:08:57 -0300

    css do modal

--- teste/src/App.js
+++ teste/src/App.js
@@ -33,6 +33,9 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
           <Route path="/user-register" element={<UserRegister />} />
           <Route path="/admin/product-register" element={isAdmin() ? <ProductRegister /> : <Navigate to="/" />} />
+          {/* <Route path="/admin/product-update" element={isAdmin() ? <ProductUpdate /> : <Navigate to="/" />} /> */}
+          {/* <Route path="/admin/product-index" element={isAdmin() ? <Productindex /> : <Navigate to="/" />} /> */}
+          
           <Route path="/admin/banner" element={isAdmin() ? <BannerCadastro /> : <Navigate to="/" />} />
           <Route path="/admin" element={isAdmin() ? <Admin /> : <Navigate to="/" />} />
         </Routes>
