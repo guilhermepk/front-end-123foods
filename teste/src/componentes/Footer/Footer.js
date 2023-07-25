15e1108e6b3c696674a52fb0cd94cbc65addc88b
Gabriel Taschner Eddine <121367742+zTaschner@users.noreply.github.com>
2023-07-25 12:33:48 -0300

    terminando o footer

--- teste/src/componentes/Footer/Footer.js
+++ teste/src/componentes/Footer/Footer.js
@@ -2,7 +2,7 @@ import './Footer.css';
 import {MdPix} from 'react-icons/md';
 import {FaCcMastercard} from 'react-icons/fa';
 import {FaCcVisa} from 'react-icons/fa';
-import {FaRegCreditCard} from 'react-icons/fa'
+import {FaRegCreditCard} from 'react-icons/fa';
 
 function Footer(){
     return(
@@ -10,47 +10,45 @@ function Footer(){
             <div className=''>
                 <section className='grid-footer'>
                 <li>
+                <div>
                 <ul className="primeiro">
                     <h2>Atendimento ao Cliente</h2>
                     <a className="footer-link">Como comprar?</a>
                     <a className="footer-link">Problemas com Entrega</a>
                     <a className="footer-link">Pedidos, Cancelamento, Trocas e Devoluções</a>
                     <a className="footer-link">Problemas com Entrega</a>
-                    <a className="footer-link">Pedidos, Cancelamento, Trocas e Devoluções</a>
                 </ul>
+                </div>
+                <div>
                 <ul className="primeiro">
                     <h2>Conheça-nos</h2>
                     <a className="footer-link">Nossa História</a>
                     <a className="footer-link">Nossa Loja</a>
                 </ul>
-
+                </div>
+                <div>
                 <ul className="primeiro">
                     <h2>Contato</h2>
                     <a className="footer-link">faleconosco@123foods.com</a>
                     <a className="footer-link">Trabalhe Conosco</a>
                 </ul>
-
+                </div>
+                <div className="pagamento">
                 <ul className="primeiro">
-                    <div className="pagamento">
                     <h2>Pagamento</h2>
-                        <a className="footer-cash">
-                        <MdPix className="pix"/>
-                        </a>
                     <a className="footer-cash">
-                        <FaCcMastercard className="mastercard"/>
-                        </a>
-                    <a className="footer-cash">
-                        <FaCcVisa className="visa"/>
-                        </a>
-                        <a className="footer-cash">
-                        <FaRegCreditCard className="cartao"/>
-                        </a>
-                    </div>
+                        <img className='img-cash' src="/imagens/icons.png"></img>
+                    </a>
                 </ul>
-                
+                </div>
+                <div>
                 <ul className="primeiro">
                     <h2>Siga-nos nas redes sociais</h2>
+                    <a>
+                        <img className='face' src="/imagens/face.png"></img>
+                    </a>
                 </ul>
+                </div>
                 </li>
                 </section>
             </div>
