3b9ce01e78c983c6eae788b4245cfefd05c31d91
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-08-02 11:15:58 -0300

    a

--- teste/src/componentes/ProductLister/ProductLister.js
+++ teste/src/componentes/ProductLister/ProductLister.js
@@ -1,9 +1,11 @@
 const ProductLister = () => {
+    let content = null;
 
     fetch('http://localhost:3000/foods')
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
+        content = data;
     });
 }
 
