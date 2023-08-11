7be6b6daa6e9b0cc38cbab8baae7747863f09dc8
viniggj2005 <102488557+viniggj2005@users.noreply.github.com>
2023-08-11 10:46:48 -0300

    navbaradm

--- teste/src/componentes/ProductLister/ProductLister.js
+++ teste/src/componentes/ProductLister/ProductLister.js
@@ -1,11 +1,56 @@
-const ProductLister = () => {
-    let content = null
+import React, { useState, useEffect } from "react";
+import { Card, Col, Container, Pagination, Row } from "react-bootstrap";
 
-    fetch('http://localhost:3000/foods')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-    });
+const ProductLister = () => {
+    const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 9;
+
+    useEffect(() => {
+        fetch('http://localhost:3000/foods')
+            .then((response) => response.json())
+            .then((data) => {
+                setProducts(data);
+            });
+    }, []);
+
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+    return (
+        <Container>
+            <Row>
+                {currentProducts.map(product => (
+                    <Col key={product.id} xs={12} md={4} className="mb-4">
+                        <Card>
+                            <Card.Img variant="top" src={`http://localhost:3000/uploads/${product.images[0]?.path}`}
+                                      onLoad={() => console.log(`Imagem carregada: /uploads/${product.images[0]?.path}`)}
+                                      onError={() => console.log(`Erro ao carregar a imagem: /uploads/${product.images[0]?.path}`)}/>
+                            <Card.Body>
+                                <Card.Title>{product.name}</Card.Title>
+                                <Card.Text>{product.price}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            <Pagination className="justify-content-center mt-4">
+                {Array.from({ length: Math.ceil(products.length / productsPerPage) }).map((_, index) => (
+                    <Pagination.Item
+                        key={index + 1}
+                        active={index + 1 === currentPage}
+                        onClick={() => paginate(index + 1)}
+                    >
+                        {index + 1}
+                    </Pagination.Item>
+                ))}
+            </Pagination>
+        </Container>
+    );
 }
 
 export default ProductLister;
--- teste/src/componentes/navbaradm/navbaradm.js
+++ teste/src/componentes/navbaradm/navbaradm.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './navbaradm.css'
-
-import {GiKnightBanner} from 'react-icons/gi';
+import{AiTwotoneNotification} from 'react-icons/ai'
+import {FaImage} from 'react-icons/fa';
 import {TbPaperBag} from 'react-icons/tb';
-
+import {RiNotificationBadgeFill}from 'react-icons/ri'
 const NavbarAdm=()=>{
+  const [minimized, setMinimized] = useState(false);
   const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+  const toggleMinimize = () => {
+    setMinimized(!minimized);
+  };
   return(
-        <div>
-      <div className="admin-container">
-        <img src={`http://localhost:3000/uploads/${userInfo.image}`} alt="User Image" className="imagem-usuario" />     <p>{userInfo.name}</p>
-        <div className="admin-sidebar">
-          <h3>Admin Dashboard</h3>
-          <ul className="admin-nav">
-            <li>
-              <Link to="/admin">Home</Link>
-            </li>
-            <li>
-              <Link to="/admin/banner"><GiKnightBanner/>Cadastro de banners</Link>
-              <Link to="/admin/bannerlist"><GiKnightBanner/>Listagem de banners</Link>
-            </li>
-            <li>
-              <Link to="/admin/product-register"><TbPaperBag/>Cadastrar Produtos</Link>
-              <Link to='/admin/product-list'> <TbPaperBag/> Lista de Produtos </Link>
+      <div>
+        <div className={`admin-container ${minimized ? 'hide-content' : ''}`}>
+          <div className={`admin-sidebar ${minimized ? 'minimized' : ''}`}>
+            <div className="user-column" onClick={toggleMinimize}>
+              <img
+                  src={`http://localhost:3000/uploads/${userInfo.image}`}
+                  alt="User Image"
+                  className={`imagem-usuario ${minimized ? 'minimized' : ''}`}
+              />
+              {!minimized && <p className="nameuser">{userInfo.name}</p>}
+            </div>
+            <h3 className={minimized ? 'hidden' : ''}>Admin Dashboard</h3>
+            <ul className={`admin-nav ${minimized ? 'hidden' : ''}`}>
+              <li>
+                <Link to="/admin">Home</Link>
+              </li>
+              <li>
+                <Link to="/admin/banner"><FaImage/>Cadastro de banners</Link>
+                <Link to="/admin/bannerlist"><FaImage/>Listagem de banners</Link>
+              </li>
+              <li>
+                <Link to="/admin/product-register"><TbPaperBag/>Cadastrar Produtos</Link>
+                <Link to='/admin/product-list'> <TbPaperBag/> Lista de Produtos </Link>
 
-            </li>
-          </ul>
-        </div>
+              </li>
+              <li>
+                <Link to="/admin/notifications"><AiTwotoneNotification/>Criar Notificação</Link>
+                <Link to='/admin/notifications-list'> <RiNotificationBadgeFill/> Lista de Notificações</Link>
 
-        <div className="admin-content">
+              </li>
+            </ul>
+          </div>
+
+          <div className={`admin-content ${minimized ? 'minimized-content' : ''}`}>
+          </div>
         </div>
-      </div>
     </div>
     )
 }
