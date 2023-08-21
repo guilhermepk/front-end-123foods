9df857befa15e7e1bb26c840e4c234f3d1f11d01
viniggj2005 <102488557+viniggj2005@users.noreply.github.com>
2023-08-21 07:12:55 -0300

    não terminado

---
+++ teste/src/componentes/UserInfo/UF.js
@@ -0,0 +1,5 @@
+class UF{
+    constructor(){}
+}
+
+export default UF;
--- teste/src/componentes/UserInfo/UserInfo.js
+++ teste/src/componentes/UserInfo/UserInfo.js
@@ -1,36 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './Navbar.css';
-import { CgProfile } from 'react-icons/cg';
-import { FiShoppingCart } from 'react-icons/fi';
-import { AiOutlineBell } from 'react-icons/ai';
-import Search from '../Search/Search';
-import Dropzone from 'react-dropzone';
-import { Eye, EyeOff } from 'react-feather';
-import { IoIosClose } from 'react-icons/io';
+import { useState, useEffect} from 'react';
+import Dropzone from "react-dropzone";
+import { Eye, EyeOff} from 'react-feather';
+import {IoIosClose} from 'react-icons/io';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-import jwt_decode from 'jwt-decode';
 
-const Navbar = () => {
+const UserInfo = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState('');
-    const [showLoginForm, setShowLoginForm] = useState(false);
-    const [showUserInfoModal, setShowUserInfoModal] = useState(false);
-    const [token, setToken] = useState(null);
-    const [decoded_token, setDecodedToken] = useState(null);
+    const [showLoginForm, setShowLoginForm] = useState(true);
+    const [showUserInfoModal, setShowUserInfoModal] = useState(true);
+    const [userInfo, setUserInfo] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [showImageUploadModal, setShowImageUploadModal] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
 
     const handleProfileClick = () => {
-        if (decoded_token) {
-            setShowUserInfoModal(!showUserInfoModal);
+        if (userInfo) {
+            setShowUserInfoModal(true);
         } else {
             setShowLoginForm(true);
         }
-    };
+    }
 
     const handleImageDrop = (acceptedFiles) => {
         setSelectedImage(acceptedFiles[0]);
@@ -38,14 +31,9 @@ const Navbar = () => {
 
     const fetchUpdatedUserInfo = async () => {
         try {
-            const response = await axios.get(`http://localhost:3000/users/${decoded_token.sub}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+            const response = await axios.get(`http://localhost:3000/users/${userInfo.id}`);
             const data = response.data;
-            console.log('Response:', response);
-            console.log('Data:', data);
+            console.log('sim')
             return data;
         } catch (error) {
             console.error('Erro ao buscar os dados do usuário:', error);
@@ -58,16 +46,17 @@ const Navbar = () => {
             const formData = new FormData();
             formData.append('file', selectedImage);
 
-            await axios.patch(`http://localhost:3000/users/${decoded_token.sub}/upload`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    Authorization: `Bearer ${token}`,
-                },
+            await axios.patch(`http://localhost:3000/users/${userInfo.id}/upload`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
             });
+
 
             const updatedUserInfo = await fetchUpdatedUserInfo();
 
             if (updatedUserInfo) {
+                setUserInfo(updatedUserInfo);
                 localStorage.setItem('userInfo', JSON.stringify(updatedUserInfo));
             }
 
@@ -77,6 +66,8 @@ const Navbar = () => {
             console.error('Erro ao enviar a imagem:', error);
         }
     };
+
+    const [showUploadButton, setShowUploadButton] = useState(false);
 
     const handleImageUploadButtonClick = () => {
         setShowImageUploadModal(true);
@@ -88,11 +79,9 @@ const Navbar = () => {
     };
 
     useEffect(() => {
-        const storedToken = localStorage.getItem('payload');
-        if (storedToken) {
-            setToken(storedToken);
-            const decodedToken = jwt_decode(storedToken);
-            setDecodedToken(decodedToken);
+        const storedUserInfo = localStorage.getItem('userInfo');
+        if (storedUserInfo) {
+            setUserInfo(JSON.parse(storedUserInfo));
         }
     }, []);
 
@@ -106,9 +95,8 @@ const Navbar = () => {
 
     const handleLogout = () => {
         setIsLoggedIn(false);
-        setToken(null);
-        setDecodedToken(null);
-        localStorage.removeItem('payload');
+        setUserInfo(null);
+        localStorage.removeItem('userInfo');
     };
 
     const handleLoginFormSubmit = async (event) => {
@@ -119,19 +107,16 @@ const Navbar = () => {
                 password,
             });
             const data = response.data;
-            const token = data.access_token;
-            console.log('Token:', token);
             console.log('Sucesso no Login:', data);
             setError('');
             setShowLoginForm(false);
-            setToken(token);
-            setDecodedToken(jwt_decode(token));
-            localStorage.setItem('payload', token);
+            setUserInfo(data);
             setShowUserInfoModal(true);
+            localStorage.setItem('userInfo', JSON.stringify(data));
             Swal.fire('Bem vindo', 'Login bem sucedido', 'success');
         } catch (error) {
-            setError('Email ou Senha incorretos');
-            Swal.fire('Ops...', 'Erro ao completar o login...', 'error');
+            setError ('Email ou Senha incorretos');
+            Swal.fire('Ops...', 'Erro ao completar o login...', 'error'); 
         }
 
         setUsername('');
@@ -141,40 +126,11 @@ const Navbar = () => {
     const closeModal = () => {
         setShowLoginForm(false);
         setShowUserInfoModal(false);
-    };
+      };
 
-
-  return (
-    <div className="container-fluid">
-      <nav className="navbar">
-        <ul className="nav-list">
-          <div className="logo">
-            <a href=''> <img src="./imagens/logoSemFundo.png" alt="Logo" /> </a>
-          </div>
-          <div className="categorias">
-      </div>  
-              <Search/>
-          <div className="conteudo">
-          {userInfo && userInfo.admin && (
-              <a href="/admin"><button className="botao-admin"> Admin </button></a>
-            )}
-           
-            <a href="#" onClick={handleProfileClick}>
-              <CgProfile className="perfil"/>
-            </a>
-
-            <a href="">
-              <FiShoppingCart className="carrinho"/>
-            </a>
-
-            <a href="">
-              <AiOutlineBell className="notificacao"/>
-            </a>
-            
-          </div>
-        </ul>
-      </nav>
-      {showLoginForm && (
+    return (
+        <div>
+            {showLoginForm && (
                 <div className="modal">
                     <div className="modal-content">
                         <IoIosClose className="close" onClick={closeModal}/>
@@ -209,87 +165,30 @@ const Navbar = () => {
                             <p className='conta-possuir'>Não possui conta? </p>
 
                             <a href='/user-register' target='_blank'> Registre-se agora! </a>
+
                         </form>
                     </div>
                 </div>
             )}
             {showUserInfoModal && userInfo && (
-                <div className="modal-usuario">
-                    <div className="modal-usuario1">
-                        <button className="botao-perfil">
-                            Meu perfil
-                        </button>
-                        <button className="botao-compras">
-                            Minhas compras
-                        </button>
-                        <button className="botao-historico">
-                            Histórico de Compras
-                        </button>
-                        <button className="botao-enderecos">
-                            Endereços Cadastrados
-                        </button>
-                        <div className="botao-sair-top">
-                        <button className="botao-sair" onClick={handleLogout}>Sair</button>
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>
+                            &times;
+                        </span>
+                        <h2 className="perfil-usuario">Informações do Usuário</h2>
+                        <div className="conta-usuario">
+                        <p>Nome: {userInfo.name}</p>
+                        <p>Email: {userInfo.email}</p>
+                        <p>Telefone: {userInfo.phone}</p>
+                        <p>CPF: {userInfo.cpf}</p>
+                        <p>Endereço: {userInfo.street}</p>
                         </div>
-                    </div>
-                   <div className="modal-conteudo">
-                        <h2 className="perfil-usuario">
-                            Olá, {userInfo.name}</h2>
                         <div className="user-image-container" onMouseEnter={() => setShowUploadButton(true)} onMouseLeave={() => setShowUploadButton(false)}>
-                            {userInfo.image && (
-                                <img src={`/uploads/${userInfo.image}`} alt="User Image" className="imagem-perfil" />
-                            )}
-                            {!userInfo.image && (
-                                <img src={`/uploads/imagem-padrao.gif`} alt="Default User Image" className="imagem-perfil" />
-                            )}
+                            <img src={`http://localhost:3000/uploads/${userInfo.image}`} alt="User Image" className="imagem-usuario" />
                             {showUploadButton && (
                                 <button className="botao-hover" onClick={handleImageUploadButtonClick}>Atualizar Imagem</button>
                             )}
-                        </div>
-                        <div className="conta-usuario">
-                        <label className="label-dados">
-                            Nome:
-                        </label>
-                        <input
-                        className="dados-pessoais" type='text'
-                        //value={userInfo.name}
-                        disabled
-                        />
-                        <label className="label-dados">
-                            Email:
-                        </label>
-                        <input
-                        className="dados-pessoais" type='text'
-                        //value={userInfo.email}
-                        disabled
-                        />
-                        <label className="label-dados">
-                            Telefone:
-                        </label>
-                        <input
-                        className="dados-pessoais" type='text'
-                        //value={userInfo.phone}
-                        disabled
-                        />
-                        <label className="label-dados">
-                            CPF:
-                        </label>
-                        <input
-                        className="dados-pessoais" type='text'
-                        //value={userInfo.cpf}
-                        disabled
-                        />
-                        <button className="button-alterar">
-                            Alterar informações
-                        </button>
-                        {/* <label>
-                            Endereço:
-                        </label>
-                        <input
-                        className="dados-pessoais" type='text'
-                        value={userInfo.street}
-                        disabled
-                        /> */}
                         </div>
                         {showImageUploadModal && (
                             <div  className="image-upload-modal">
@@ -313,16 +212,17 @@ const Navbar = () => {
                                                 </div>
                                             )}
                                         </Dropzone>
-                                        <button type="submit">Salvar</button>
+                                        <button type={"submit"}>Salvar</button>
                                     </form>
                             </div>
                             </div>
                         )}
+                        <button onClick={handleLogout}>Logout</button>
                     </div>
                 </div>
             )}
-    </div>
-  );
-};
-  
-export default Navbar;
+        </div>
+    );
+}
+
+export default UserInfo;
