e9fe2831ea186066d3aa464aa225d44098e9045d
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2023-08-22 16:36:11 -0300

    mexi na paginação dos produtos da página inicial

--- teste/src/componentes/HomeProductLister/HomeProductLister.js
+++ teste/src/componentes/HomeProductLister/HomeProductLister.js
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from "react";
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card'
 
-// import "bootstrap/dist/css/bootstrap.min.css";
 import './HomeProductLister.css'
-import ProductPage from "../ProductPage/ProductPage";
+import { Pagination } from "react-bootstrap";
 
-const HomeProductLister = () => {
+const  HomeProductLister = () => {
     const [products, setProducts] = useState([]);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 4;
+
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+        for(let x = 0; x < pages.length; x++){
+            if (pages[x] === currentPage){
+                console.log('aaaaa', pages[x])
+            }
+        }
+    };
+
+    const pages = Array.from({ length: Math.ceil(products.length / productsPerPage) });
 
     useEffect(() => {
         fetch('http://localhost:3000/foods')
@@ -17,34 +34,45 @@ const HomeProductLister = () => {
             });
     }, []);
 
-    const card = document.getElementsByClassName('cardProduct');
-    card.onClick = (productId) => {
-        //window.location.assign(`/product/${productId}`)
-        console.log(`clicaram no id ${productId}`)
-    };
+    const Pag = () => {
+        return (
+            <Pagination className="pages">
+                {pages.map((_, index) => (
+                    <Pagination.Item className="page"
+                        key={index + 1}
+                        onClick={() => paginate(index + 1)}
+                    >
+                        {index + 1}
+                    </Pagination.Item>
+                ))}
+            </Pagination>
+        );
+    }
 
     return (
-        <div>
+        <div className="divList">
+            <Pag/>
             <li className='cardList'>
-            {products.map((product, index) => (
-                <Col key={index}>
-                    <Card className="cardProduct">
-                        <a href={`/product/${product.id}`}>
-                            <div className="divImg">
-                                <Card.Img className="cardImg"
-                                    src={`http://localhost:3000/uploads/${product.images[0]?.path}`}
-                                    onLoad={() => console.log(`Imagem carregada: /uploads/${product.images[0]?.path}`)}
-                                    onError={() => console.log(`Erro ao carregar a imagem: /uploads/${product.images[0]?.path}`)}
-                                />
-                                <p>{product.name}</p>
-                                <p>{product.brand}</p>
-                                <p>R$ {product.price}</p>
-                            </div>
-                        </a>
-                    </Card>
-                </Col>
-            ))}
+                {currentProducts.map((product, index) => (
+                    <Col key={index}>
+                        <Card className="cardProduct">
+                            <a href={`/product/${product.id}`}>
+                                <div className="divImg">
+                                    <Card.Img className="cardImg"
+                                        src={`http://localhost:3000/uploads/${product.images[0]?.path}`}
+                                        onLoad={() => console.log(`Imagem carregada: /uploads/${product.images[0]?.path}`)}
+                                        onError={() => console.log(`Erro ao carregar a imagem: /uploads/${product.images[0]?.path}`)}
+                                    />
+                                    <p>{product.name}</p>
+                                    <p>{product.brand}</p>
+                                    <p>R$ {product.price}</p>
+                                </div>
+                            </a>
+                        </Card>
+                    </Col>
+                ))}
             </li>
+            <Pag/>
         </div>
     );
 }
