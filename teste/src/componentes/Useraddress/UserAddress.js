6d8103193d5b4cb7f02e1e211cd4dc539f3cbff0
Emanuelle Vodiani <manuvodi@hotmail.com>
2023-08-25 12:08:42 -0300

    funcionando

--- teste/src/componentes/Useraddress/UserAddress.js
+++ teste/src/componentes/Useraddress/UserAddress.js
@@ -39,93 +39,96 @@ const UserAddress = ({handleregisterAddress}) => {
             });
             const data = response.data;
             setUseraddress(data);
+            console.log('useraddress:', data);
         } catch (error) {
             console.error('Erro ao buscar os dados do usuário:', error);
             setUseraddress(null);
         }
     };
-console.log('useraddress',useraddress)
+
     return(
 <div>
         {userId !== null && (
-            <div>
+            <div><h3 className="titulo-endereco">Endereço 1</h3>
+                        <div className="endereco-container" key={useraddress[0].id}>
+                            <label className="label-endereco">
+                                Cidade:
+                            </label>
+                            <input
+                            className="dados-endereco" type='text'
+                            value={useraddress[0].city}
+                            disabled
+                            />
+                            <label className="label-endereco">
+                                Rua:
+                            </label>
+                            <input
+                            className="dados-endereco" type='text'
+                            value={useraddress[0].street}
+                            disabled
+                            />
+                            <label className="label-endereco">
+                                Estado:
+                            </label>
+                            <input
+                            className="dados-endereco" type='text'
+                            value={useraddress[0].state}
+                            disabled
+                            />
+                            <label className="label-endereco">
+                                CEP:
+                            </label>
+                            <input
+                            className="dados-endereco" type='text'
+                            value={useraddress[0].cep}
+                            disabled
+                            />
+                            <label className="label-endereco">
+                                Bairro:
+                            </label>
+                            <input
+                            className="dados-endereco" type='text'
+                            value={useraddress[0].district}
+                            disabled
+                            />
+                            <label className="label-endereco">
+                                Complemento:
+                            </label>
+                            <input
+                            className="dados-endereco" type='text'
+                            value={useraddress[0].complement}
+                            disabled
+                            />
+                            <label className="label-endereco">
+                                Número:
+                            </label>
+                            <input
+                            className="dados-endereco" type='text'
+                            value={useraddress[0].numberhouse}
+                            disabled
+                            />
+                        </div>
                 {useraddress && useraddress.length === 1 && (
-
-                    <div className="endereco" key={useraddress[0].id}>
-                        <h3 className="numero-endereco">Endereço 1</h3>
-                        <label className="label-endereco">
-                            Cidade:
-                        </label>
-                        <input
-                        className="dados-endereco" type='text'
-                        value={useraddress[0].city}
-                        disabled
-                        />
-                        <label className="label-endereco">
-                            Rua:
-                        </label>
-                        <input
-                        className="dados-endereco" type='text'
-                        value={useraddress[0].street}
-                        disabled
-                        />
-                        <label className="label-endereco">
-                            Estado:
-                        </label>
-                        <input
-                        className="dados-endereco" type='text'
-                        value={useraddress[0].state}
-                        disabled
-                        />
-                        <label className="label-endereco">
-                            CEP:
-                        </label>
-                        <input
-                        className="dados-endereco" type='text'
-                        value={useraddress[0].cep}
-                        disabled
-                        />
-                        <label className="label-endereco">
-                            Bairro:
-                        </label>
-                        <input
-                        className="dados-endereco" type='text'
-                        value={useraddress[0].district}
-                        disabled
-                        />
-                        <label className="label-endereco">
-                            Complemento:
-                        </label>
-                        <input
-                        className="dados-endereco" type='text'
-                        value={useraddress[0].complement}
-                        disabled
-                        />
-                        <label className="label-endereco">
-                            Número:
-                        </label>
-                        <input
-                        className="dados-endereco" type='text'
-                        value={useraddress[0].numberhouse}
-                        disabled
-                        />
-                        <button onClick={handleregisterAddress}>Adicionar Endereço</button>
-
-                    </div>
+                    <div className="endereco-um">    
+                        
+                        <button className="botao-adicionar" onClick={handleregisterAddress}>Adicionar Endereço</button>
+                    </div>    
                 )}
 
                 {useraddress && useraddress.length > 1 && (
                     useraddress.map((address, index) => (
-                        <div key={address.id}>
-                            <h3>Endereço {index + 1}</h3>
-                            <p>Cidade: {address.city}</p>
-                            <p>Rua: {address.street}</p>
-                            <p>Estado: {address.state}</p>
-                            <p>Cep: {address.cep}</p>
-                            <p>Bairro: {address.district}</p>
-                            <p>Complemento: {address.complement}</p>
-                            <p>Número: {address.numberhouse}</p>
-                        </div>
+                        <div className="endereco-dois">
+                            <div key={address.id}>
+                                <h3>Endereço {index + 1}</h3>
+                                <p>Cidade: {address.city}</p>
+                                <p>Rua: {address.street}</p>
+                                <p>Estado: {address.state}</p>
+                                <p>Cep: {address.cep}</p>
+                                <p>Bairro: {address.district}</p>
+                                <p>Complemento: {address.complement}</p>
+                                <p>Número: {address.numberhouse}</p>
+                            </div>
+                        </div>    
                     ))
                 )}
 
@@ -133,6 +136,9 @@ console.log('useraddress',useraddress)
                     <div>
                         <button onClick={handleregisterAddress}>Adicionar Endereço</button>
                     </div>
+                    <div>
+                    <button onClick={handleregisterAddress}>Adicionar Endereço</button>
+                </div>
                 )}
             </div>
         )}
