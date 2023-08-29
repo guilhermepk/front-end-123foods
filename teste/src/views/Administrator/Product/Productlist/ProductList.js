e0232f44ce8e9438f9d377617ce8b14976db4e1c
Gabriel Taschner Eddine <121367742+zTaschner@users.noreply.github.com>
2023-08-29 12:36:54 -0300

    arrumações

--- teste/src/views/Administrator/Product/Productlist/ProductList.js
+++ teste/src/views/Administrator/Product/Productlist/ProductList.js
@@ -1,9 +1,11 @@
 import Productlister from '../../../../componentes/Productlister/Productlister';
 import { Link } from "react-router-dom";
+import NavigationbarAdm from '../../../../componentes/NavigationbarAdm/NavigationbarAdm';
 
 const Productlist = () => {
     return (
         <div>
+            <NavigationbarAdm />
             <Link to="/admin"> Admin Home</Link>
             <h1> Produtos </h1>
             <Productlister/>
