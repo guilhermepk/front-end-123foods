e085465fa519b6c54594cc452c3f28106480e2bd
Vinicius Gabriel Graupmann Juraszek <102488557+viniggj2005@users.noreply.github.com>
2023-09-01 09:03:48 -0300

    pagina myshopping e purchase historic

--- teste/src/componentes/Myshopping/Myshopping.js
+++ teste/src/componentes/Myshopping/Myshopping.js
@@ -1,10 +1,71 @@
-const Myshopping=()=>{
+import React, { useState, useEffect } from 'react';
+import { useUserinfo } from '../Userinfo/Userinfo';
+import jwt_decode from 'jwt-decode';
 
-    return(
-        <div>
-            <h1>Minhas Compras</h1>
+const Myshopping = () => {
+  const [token, setToken] = useState(null);
+  const [decodedToken, setDecodedToken] = useState(null);
+  const [data, setData] = useState([]);
+  const [groupedProducts, setGroupedProducts] = useState({});
+  const userId = decodedToken?.sub;
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('payload');
+    if (storedToken) {
+      setToken(storedToken);
+      const decodedToken = jwt_decode(storedToken);
+      setDecodedToken(decodedToken);
+    }
+  }, []); 
+
+  useEffect(() => {
+    if (userId) {
+      fetch(`http://localhost:3000/purchases/${userId}/comprado`)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        })
+        .catch((error) => {
+          console.error('Erro ao buscar dados:', error);
+        });
+    }
+  }, [userId]);
+
+  useEffect(() => {
+    const groupedProducts = {};
+    data.forEach((product) => {
+      const updatedAt = new Date(product.updatedAt);
+      const key = updatedAt.toISOString().substring(0, 16);
+      if (!groupedProducts[key]) {
+        groupedProducts[key] = {
+          date: updatedAt,
+          products: [],
+        };
+      }
+      groupedProducts[key].products.push(product);
+    });
+    setGroupedProducts(groupedProducts);
+  }, [data]);
+
+  return (
+    <div>
+      <h1>Minhas Compras</h1>
+      {Object.values(groupedProducts).map((group) => (
+        <div key={group.date}>
+        <h4>Comprado dia: {group.date.getDate()}/{group.date.getMonth() + 1}</h4>
+          <ul>
+            {group.products.map((product) => (
+              <li key={product.id}>
+                <img src={`http://localhost:3000/uploads/${product.image.path}`} alt="Imagem da compra" />
+                <p>Status: {product.status}</p>
+                <p>Quantidade: {product.amount}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Myshopping;
--- teste/src/componentes/purchaseshistoric/purchaseshistoric.js
+++ teste/src/componentes/purchaseshistoric/purchaseshistoric.js
@@ -1,10 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { useUserinfo } from '../Userinfo/Userinfo';
+import jwt_decode from 'jwt-decode';
 const Purchaseshistoric=()=>{
-
-
-    return(
+      const [token, setToken] = useState(null);
+      const [decodedToken, setDecodedToken] = useState(null);
+      const [data, setData] = useState([]);
+      const [groupedProducts, setGroupedProducts] = useState({});
+      const userId = decodedToken?.sub;
+    
+      useEffect(() => {
+        const storedToken = localStorage.getItem('payload');
+        if (storedToken) {
+          setToken(storedToken);
+          const decodedToken = jwt_decode(storedToken);
+          setDecodedToken(decodedToken);
+        }
+      }, []); 
+    
+      useEffect(() => {
+        if (userId) {
+          fetch(`http://localhost:3000/purchases/${userId}/entregue`)
+            .then((response) => response.json())
+            .then((data) => {
+              setData(data);
+            })
+            .catch((error) => {
+              console.error('Erro ao buscar dados:', error);
+            });
+        }
+      }, [userId]);
+    
+      useEffect(() => {
+        const groupedProducts = {};
+        data.forEach((product) => {
+          const updatedAt = new Date(product.updatedAt);
+          const key = updatedAt.toISOString().substring(0, 16);
+          if (!groupedProducts[key]) {
+            groupedProducts[key] = {
+              date: updatedAt,
+              products: [],
+            };
+          }
+          groupedProducts[key].products.push(product);
+        });
+        setGroupedProducts(groupedProducts);
+      }, [data]);
+    
+      return (
         <div>
-            <h1>Historico de compras</h1>
+          <h1>Minhas Compras</h1>
+          {Object.values(groupedProducts).map((group) => (
+            <div key={group.date}>
+            <h4>Entregue dia: {group.date.getDate()}/{group.date.getMonth() + 1}</h4>
+              <ul>
+                {group.products.map((product) => (
+                  <li key={product.id}>
+                    <img src={`http://localhost:3000/uploads/${product.image.path}`} alt="Imagem da compra" />
+                    <p>Status: {product.status}</p>
+                    <p>Quantidade: {product.amount}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
-    )
-}
+      );
+    }
 export default Purchaseshistoric;
