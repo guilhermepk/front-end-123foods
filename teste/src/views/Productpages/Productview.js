0227dc972754e29bbaeb35de297d6aa5cdffa550
Vinicius Gabriel Graupmann Juraszek <102488557+viniggj2005@users.noreply.github.com>
2023-09-19 10:10:32 -0300

    pastas do front organizadas

--- teste/src/views/Productpages/Productview.js
+++ teste/src/views/Productpages/Productview.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import Footer from "../../componentes/Footer/Footer"
 import Productpage from "../../componentes/Productpage/Productpage";
-import Navigationbar from '../../componentes/Navigationbar/Navigationbar';
+import Navigationbar from '../../componentes/ComponentsofNavbar/Navigationbar/Navigationbar';
 
 const Productview = () => {
     const { productId } = useParams();
