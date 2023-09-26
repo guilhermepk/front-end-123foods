f8373acf78eacc67bb46102a18fcee5e938cbcec
Gabriel Taschner Eddine <121367742+zTaschner@users.noreply.github.com>
2023-09-26 08:06:56 -0300

    conheça o nosso time

--- teste/src/views/Footerpages/Team.js
+++ teste/src/views/Footerpages/Team.js
@@ -3,8 +3,10 @@ import Navigationbar from '../../componentes/ComponentsofNavbar/Navigationbar/Na
 import {TbPhotoHeart} from 'react-icons/tb';
 import {BsTwitch} from 'react-icons/bs';
 import {BsInstagram} from 'react-icons/bs';
-import {BsGithub} from 'react-icons/bs'
-import {RiComputerLine} from 'react-icons/ri'
+import {BsGithub} from 'react-icons/bs';
+import {RiComputerLine} from 'react-icons/ri';
+import {TbPointerHeart} from 'react-icons/tb';
+import {FiFigma} from 'react-icons/fi';
 import './Team.css'
 
 const Team = () =>{
@@ -18,6 +20,45 @@ const Team = () =>{
         <u className="front-title">Front-end</u>
         <u className="back-title">Back-end</u>
         <u className="design-title">Design</u>
+        </div>
+        <div className="front-end-title">
+            <TbPointerHeart/>
+            <p>
+                Design
+            </p>
+        </div>
+        <div className="design">
+        <div className="front-end-card">
+                <div className="front-end-div">
+                <img className="front-end-img" src="https://avatars.githubusercontent.com/u/118612780?v=4" />
+                </div>
+            <div className="front-end-subtitle">
+                <p className="front-end-nome">Manu Vodiane</p>
+                <p>Front-end</p>
+                <p>Designer</p>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a className="twitch" href="https://github.com/ManuVodi"><BsGithub /></a>
+                    <a href="https://www.instagram.com/manu_vodi/"><BsInstagram/></a>
+                </div>
+                </div>
+            </div>
+            <div className="front-end-card">
+                <div className="front-end-div">
+                <img className="front-end-img" src="imagens/gabriel.jpeg" />
+                </div>
+            <div className="front-end-subtitle">
+                <p className="front-end-nome">Gabriel Taschner Eddine</p>
+                <p>Front-end</p>
+                <p>Designer</p>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a href="https://www.twitch.tv/ztaschner"><BsTwitch /></a>
+                    <a className="twitch" href="https://github.com/zTaschner"><BsGithub /></a>
+                    <a href="https://www.instagram.com/gabriel_taschner/"><BsInstagram/></a>
+                </div>
+                </div>
+            </div>
         </div>
         <div className="front-end-title">
         <TbPhotoHeart/>
@@ -130,6 +171,10 @@ const Team = () =>{
                 </div>
                 </div>
             </div> 
+            </div>
+            <div></div>
+            <div className="figma">
+            <a href="https://www.figma.com/proto/5BkUoGKTSGbkcWKdGgpeV8/123Foods?node-id=1-5&starting-point-node-id=1%3A5" className="figma-link"><FiFigma/></a>
             </div>
         <Footer />
         </div>
