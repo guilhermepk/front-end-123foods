4633297d4398db8a914634c48f59b96c964bc0ec
Gabriel Taschner Eddine <gabrieltaschnereddine@gmail.com>
2023-09-25 22:28:46 -0300

    fazendo o footer, mano isso tá daora

--- teste/src/views/Footerpages/Team.js
+++ teste/src/views/Footerpages/Team.js
@@ -1,6 +1,9 @@
 import Footer from "../../componentes/Footer/Footer"
 import Navigationbar from '../../componentes/ComponentsofNavbar/Navigationbar/Navigationbar';
 import {TbPhotoHeart} from 'react-icons/tb';
+import {BsTwitch} from 'react-icons/bs';
+import {BsInstagram} from 'react-icons/bs';
+import {BsGithub} from 'react-icons/bs'
 import './Team.css'
 
 const Team = () =>{
@@ -15,14 +18,14 @@ const Team = () =>{
         <u>Back-end</u>
         <u>Design</u>
         </div>
-        <div className="front-end">
         <div className="front-end-title">
         <TbPhotoHeart/>
-            <p>
+            <p className="label-front-end">
                 Front-end
             </p>
             </div>
-            <div className="front-end-content">
+        <div className="front-end">
+            <div className="front-end-card">
                 <div className="front-end-div">
                 <img className="front-end-img" src="imagens/gabriel.jpeg" />
                 </div>
@@ -30,10 +33,38 @@ const Team = () =>{
                 <p className="front-end-nome">Gabriel Taschner</p>
                 <p>Front-end</p>
                 <p>Designer</p>
-                <hr className="sss" width="80%" color="grey"/>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a href="https://www.twitch.tv/ztaschner"><BsTwitch /></a>
+                    <a className="twitch" href="https://github.com/zTaschner"><BsGithub /></a>
+                    <a href="https://www.instagram.com/gabriel_taschner/"><BsInstagram/></a>
+                </div>
                 </div>
             </div>
+            <div className="front-end-card2">
+            <div className="front-end-div">
+                <img className="front-end-img" src="https://avatars.githubusercontent.com/u/118612780?v=4" />
+                </div>
+                <div className="front-end-subtitle">
+                <p className="front-end-nome">Manu Vodiane</p>
+                <p>Front-end</p>
+                <p>Designer</p>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a className="twitch" href="https://github.com/ManuVodi"><BsGithub /></a>
+                    <a href="https://www.instagram.com/manu_vodi/"><BsInstagram/></a>
+                </div>
+                </div>
+            </div>            
         </div>
+        <div className="front-end2">
+        <div className="front-end-card3">
+        
+            </div>
+            <div className="front-end-card3">
+        
+            </div> 
+            </div>
         <Footer />
         </div>
         
