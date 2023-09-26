f06356f0833066510eda562126616901835e3be0
Gabriel Taschner Eddine <gabrieltaschnereddine@gmail.com>
2023-09-25 23:25:48 -0300

    mais footer

--- teste/src/views/Footerpages/Team.js
+++ teste/src/views/Footerpages/Team.js
@@ -4,6 +4,7 @@ import {TbPhotoHeart} from 'react-icons/tb';
 import {BsTwitch} from 'react-icons/bs';
 import {BsInstagram} from 'react-icons/bs';
 import {BsGithub} from 'react-icons/bs'
+import {RiComputerLine} from 'react-icons/ri'
 import './Team.css'
 
 const Team = () =>{
@@ -14,9 +15,9 @@ const Team = () =>{
         <h1>Conheça o Nosso Time</h1>
         </div>
         <div className="Team-subtiltle">
-        <u>Front-end</u>
-        <u>Back-end</u>
-        <u>Design</u>
+        <u className="front-title">Front-end</u>
+        <u className="back-title">Back-end</u>
+        <u className="design-title">Design</u>
         </div>
         <div className="front-end-title">
         <TbPhotoHeart/>
@@ -30,7 +31,7 @@ const Team = () =>{
                 <img className="front-end-img" src="imagens/gabriel.jpeg" />
                 </div>
             <div className="front-end-subtitle">
-                <p className="front-end-nome">Gabriel Taschner</p>
+                <p className="front-end-nome">Gabriel Taschner Eddine</p>
                 <p>Front-end</p>
                 <p>Designer</p>
                 <hr className="front-end-line" width="80%" color="grey"/>
@@ -43,7 +44,9 @@ const Team = () =>{
             </div>
             <div className="front-end-card2">
             <div className="front-end-div">
+            <div className="front-end-div">
                 <img className="front-end-img" src="https://avatars.githubusercontent.com/u/118612780?v=4" />
+                </div>
                 </div>
                 <div className="front-end-subtitle">
                 <p className="front-end-nome">Manu Vodiane</p>
@@ -59,10 +62,73 @@ const Team = () =>{
         </div>
         <div className="front-end2">
         <div className="front-end-card3">
-        
+        <div className="front-end-div">
+            <div className="front-end-div">
+                <img className="front-end-img" src="https://avatars.githubusercontent.com/u/112448450?v=4" />
+                </div>
+                </div>
+                <div className="front-end-subtitle">
+                <p className="front-end-nome">Guilherme Prigol</p>
+                <p>Back-end</p>
+                <p>Front-end</p>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a className="twitch" href="https://github.com/guilhermepk"><BsGithub /></a>
+                    <a href="https://www.instagram.com/o_guilherme_la/"><BsInstagram/></a>
+                </div>
+                </div>
             </div>
             <div className="front-end-card3">
-        
+            <div className="front-end-div">
+                <img className="front-end-img" src="https://avatars.githubusercontent.com/u/102488557?v=4"/>
+                </div>
+                <div className="front-end-subtitle">
+                <p className="front-end-nome">Vinicius Gabriel Graupmann</p>
+                <p>Back-end</p>
+                <p>Front-end</p>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a className="twitch" href="https://github.com/viniggj2005"><BsGithub /></a>
+                    <a href="https://www.instagram.com/vinicius_graupmann/"><BsInstagram/></a>
+                </div>
+                </div>
+            </div> 
+            </div>
+            <div className="label-back-end">
+                <RiComputerLine />
+                <p>Back-end</p>
+                
+            </div>
+            <div className="back-end">
+                <div className="front-end-card4">
+            <div className="front-end-div">
+                <img className="front-end-img" src="https://avatars.githubusercontent.com/u/112448450?v=4"/>
+                </div>
+                <div className="front-end-subtitle">
+                <p className="front-end-nome">Guilherme Prigol</p>
+                <p>Back-end</p>
+                <p>Front-end</p>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a className="twitch" href="https://github.com/guilhermepk"><BsGithub /></a>
+                    <a href="https://www.instagram.com/o_guilherme_la/"><BsInstagram/></a>
+                </div>
+                </div>
+                </div>
+                <div className="front-end-card4">
+            <div className="front-end-div">
+                <img className="front-end-img" src="https://avatars.githubusercontent.com/u/102488557?v=4"/>
+                </div>
+                <div className="front-end-subtitle">
+                <p className="front-end-nome">Vinicius Gabriel Graupmann</p>
+                <p>Back-end</p>
+                <p>Front-end</p>
+                <hr className="front-end-line" width="80%" color="grey"/>
+                <div className="front-end-icons">
+                    <a className="twitch" href="https://github.com/viniggj2005"><BsGithub /></a>
+                    <a href="https://www.instagram.com/vinicius_graupmann/"><BsInstagram/></a>
+                </div>
+                </div>
             </div> 
             </div>
         <Footer />
