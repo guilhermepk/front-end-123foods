efeb8168c6659939d4ad3401d4ba189710d471dd
Gabriel Taschner Eddine <121367742+zTaschner@users.noreply.github.com>
2023-09-27 12:19:49 -0300

    Footer praticamente terminado

--- teste/src/views/Footerpages/WorkwithUs.js
+++ teste/src/views/Footerpages/WorkwithUs.js
@@ -1,19 +1,29 @@
 import './WorkwithUs.css'
+import {AiFillHeart} from 'react-icons/ai';
 import Footer from "../../componentes/Footer/Footer"
 import Navigationbar from '../../componentes/ComponentsofNavbar/Navigationbar/Navigationbar';
 const WorkwithUs= () => {
 
-
     return (
-        <div className="Workwithus">
+        <div>
             <header><Navigationbar/></header>
             <div>
       <div className="work-title">
-        <h2>Trabalhe conosco</h2>
+        <h1>Trabalhe conosco</h1>
       </div>
-      <img src="/imagens/timefoda.jpeg"/>
+      <div className="work-subtitle">
+        <p>
+          Um agradecimento especial a essa equipe incrível 
+        </p>
+      </div>
+      <div className="time-img-div">
+        <img className="time-img" src="imagens/timefoda.jpg"/>
+      </div>
+      <div className="heart-icon">
+        <AiFillHeart/>
+      </div>
     </div>
-            <Footer />
+          <Footer />
         </div>
     );
 }
