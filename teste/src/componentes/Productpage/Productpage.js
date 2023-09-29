e8fa56ab649b6ac727dac7636d113398ab90c12d
Vinicius Gabriel Graupmann Juraszek <102488557+viniggj2005@users.noreply.github.com>
2023-09-29 08:59:04 -0300

    carousel

--- teste/src/componentes/Productpage/Productpage.js
+++ teste/src/componentes/Productpage/Productpage.js
@@ -31,11 +31,15 @@ const Productpage = (props) => {
         fetch(`${process.env.REACT_APP_HOST}/products/similar/${props.productId}`)
             .then((response) => response.json())
             .then((data) => {
-                setSimilar(data.slice(0, data.length));
-                console.log('data similar: ', data);
+                if (data) {
+                    setSimilar(data.slice(0, data.length));
+                    console.log('data similar: ', data);
+                } else {
+                    console.log('data similar está vazio');
+                }
             })
             .catch((error) => {
-                console.error(error);
+                console.error('Erro ao buscar dados similares:', error);
             });
     }, [props.productId]);
 
@@ -63,19 +67,19 @@ const Productpage = (props) => {
             <div className="banner">
                 <div className="product-list">
                     <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
-                        <Link to={`/product/${similar.id}`} className="linkCard">
+                        <Link to={`/product/${similar.productId}`} className="linkCard">
                             <div className="mb-3">
-                                {similar.images && (
                                     <Card.Img
                                         className="cardImg"
-                                        src={`${process.env.REACT_APP_HOST}/uploads/${similar.images[0]?.path}`}
-                                        onError={() => console.log(`Erro ao carregar a imagem: /uploads/${similar.images[0]?.path}`)}
+                                        src={`${process.env.REACT_APP_HOST}/uploads/${similar.path}`}
+                                        
+                                        alt={`Imagem do produto ${similar.name}`}
+                                        onError={() => console.log(`Erro ao carregar a imagem: /uploads/${similar.path}`)}
                                     />
-                                )}
                             </div>
                             <div>
                                 <h4 className="mb-1">{similar.name}</h4>
-                                <h6 className="mb-1">RS${similar.price}</h6>
+                                <h6 className="mb-1">R${similar.price}</h6>
                             </div>
                         </Link>
                     </div>
@@ -86,12 +90,15 @@ const Productpage = (props) => {
 
     const settings = {
         dots: true,
+        arrows:true,
         infinite: true,
         vertical: true,
         verticalSwiping: true,
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
+        onWheel: (event) => event.preventDefault(),
+    
     };
 
     return (
@@ -121,13 +128,19 @@ const Productpage = (props) => {
                 <div className="product-text">
                     <p>Itens Relacionados</p>
                 </div>
-                <Slider {...settings}>
+                <div className="carousel">
+                {similar.length > 0 ? (
+                    <Slider {...settings}>
                     {similar.map((item, index) => (
                         <div key={index} className={`carousel-item`}>
-                            {productTemplate(item)}
+                        {productTemplate(item)}
                         </div>
                     ))}
-                </Slider>
+                    </Slider>
+                ) : (
+                    <p>Nenhum produto relacionado</p>
+                )}
+                </div>
             </div>
         </div>
     );
